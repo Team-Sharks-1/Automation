d9a9518d214aaea311c96cbb2d3100727633cb03
ARMAN LAMBA <armanlamba28@gmail.com>
2024-11-08 00:36:13 -0500

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,6 +1,6 @@
 # infra/outputs.tf
-
 output "public_ip" {
   description = "The public IP address of the VM"
   value       = azurerm_public_ip.public_ip.ip_address
+  depends_on  = [azurerm_linux_virtual_machine.vm]  # Add this to ensure VM is fully created
 }
